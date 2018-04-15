827f5deb4d1f1e0657f8f8ab87b2953b304c57e4
realdennis <evolon688@gmail.com>
2018-04-16 00:52:39 +0800

    change the port

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
+let host = process.env.HOST || '0.0.0.0';
+let port = process.env.PORT || 8080;
 const http = require('http');
-let port = 8080;
 
 function createServer(){
     http.createServer((req,res) => {
         res.writeHead(200);
         res.end(req.connection.remoteAddress);
-    }).listen(port,'0.0.0.0')
+    }).listen(port,host)
 	console.log(`Server running at ${port}`);
 }
-
 createServer();
