e708afc245698b3c4e9709cf2f0321c6daac7af5
realdennis <evolon688@gmail.com>
2019-01-14 19:25:59 +0800

    update

--- server.js
+++ server.js
@@ -1,23 +1,18 @@
-let host = process.env.HOST || '0.0.0.0';
-let port = process.env.PORT || 8080;
+const HOST = process.env.HOST || '0.0.0.0';
+const PORT = process.env.PORT || 8080;
 const http = require('http');
 
-function thisIP(req){
-	let forwardIP = req.headers['x-forwarded-for']; 
-	if(forwardIP){
-		let forwardedIps = forwardIP.split(',');
-		ipAddress = forwardedIps[0];
-	}else{
-		ipAddress = req.connection.remoteAddress;
-	}
-	return ipAddress;
-}
-
-function createServer(){
-    http.createServer((req,res) => {
-        res.writeHead(200);
-        res.end(thisIP(req));
-    }).listen(port,host)
-	console.log(`Server running at ${port}`);
-}
-createServer();
+const realIP = req => {
+  const xForward = req.headers['x-forwarded-for'];
+  return xForward ? xForward.split(',')[0] : req.connection.remoteAddress;
+};
+const serve = () => {
+  http
+    .createServer((req, res) => {
+      res.writeHead(200);
+      res.end(realIP(req));
+    })
+    .listen(PORT, HOST);
+  console.log(`Server running at ${PORT}`);
+};
+serve();
